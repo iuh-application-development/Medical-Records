98dda42f8f2d0beda50c3549b404eda0447e6674
NamNam <lam2004ha@gmail.com>
2025-04-25 21:14:08 +0700

    restart project

--- app/forms/doctor_forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class NotificationForm(FlaskForm):
-    message = StringField('Message', validators=[DataRequired(), Length(max=500)])
-    submit = SubmitField('Send Notification')
