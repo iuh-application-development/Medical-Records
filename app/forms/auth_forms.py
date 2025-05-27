5c1c2389cba4bde7d82019b9f549f216fe5a79e7
tontide1 <tide.tantai@gmail.com>
2025-05-27 17:59:23 +0700

    fix lỗi phone

--- app/forms/auth_forms.py
+++ app/forms/auth_forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=80)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = StringField('Phone', validators=[DataRequired(), Length(min=10, max=15)])
+    phone = StringField('Phone', validators=[
+        DataRequired(), 
+        Length(min=10, max=10, message="Phone number must be exactly 10 digits"),
+        Regexp('^0[0-9]{9}$', message="Phone number must start with 0 and contain only digits")
+    ])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
--- app/forms/patient_forms.py
+++ app/forms/patient_forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, DateField, SubmitField, FloatField
-from wtforms.validators import DataRequired, Email, Length, NumberRange, Optional
+from wtforms.validators import DataRequired, Email, Length, NumberRange, Optional, Regexp
 
 class UpdateProfileForm(FlaskForm):
     full_name = StringField('Full Name', validators=[DataRequired(), Length(max=100)])
-    phone = StringField('Phone', validators=[DataRequired(), Length(min=10, max=15)])
+    phone = StringField('Phone', validators=[
+        DataRequired(), 
+        Length(min=10, max=10, message="Phone number must be exactly 10 digits"),
+        Regexp('^0[0-9]{9}$', message="Phone number must start with 0 and contain only digits")
+    ])
     email = StringField('Email', validators=[DataRequired(), Email()])
     avatar = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
